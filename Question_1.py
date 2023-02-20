9ba9b9e68a447cacd56cceb7bcd58b89f0fb94ad
Robert Gatt <n3rub1@gmail.com>
2023-02-20 19:50:34 +0100

    first commit

---
+++ Question_1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 20 19:46:39 2023
+
+Question 1
+"""
+
---
+++ Question_3.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 20 19:49:56 2023
+
+Question 3
+"""
+
