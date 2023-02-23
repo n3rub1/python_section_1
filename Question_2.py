c0cd13aa26ea96682c2d8fadc4c1df756abeb381
n3rub1 <52095797+n3rub1@users.noreply.github.com>
2023-02-23 10:07:28 +0100

    finished question 2

--- Question_2.py
+++ Question_2.py
@@ -5,38 +5,41 @@ Created on Mon Feb 20 19:49:40 2023
 Question 2
 """
 import random
+import matplotlib.pyplot as plt
+
 x = 0
 y = 0
-randomNumber = 0
+xValues = []
+yValues = []
 maxPoints = 90000
 
 def functionOne(x, y):
     """Function 1 - given"""
-    return (list[0,0.16*y])
+    return ([0,0.16*y])
 
 def functionTwo(x, y):
     """Function 2 - given"""
-    return (list[0.85 * x + 0.04 * y, -0.04 * x + 0.85 * y + 1.6])
+    return ([0.85 * x + 0.04 * y, -0.04 * x + 0.85 * y + 1.6])
 
 def functionThree(x, y):
     """Function 3 - given"""
-    return (list[0.2 * x - 0.26 * y, 0.23 * x + 0.22 * y + 1.6])
+    return ([0.2 * x - 0.26 * y, 0.23 * x + 0.22 * y + 1.6])
 
 def functionFour(x, y):
     """Function 4 - given"""
-    return (list[-0.15 * x + 0.28 * y, 0.26 * x + 0.24 * y + 0.44])
+    return ([-0.15 * x + 0.28 * y, 0.26 * x + 0.24 * y + 0.44])
     
-randomNumber = random.random()
+functions = [functionOne, functionTwo, functionThree, functionFour]
+probability = [0.01, 0.85, 0.07, 0.07]
+randomFunctionGenerator = random.choices(functions, probability, k = maxPoints)
 
-if(randomNumber <= 0.01):
-    functionOne(5,5)
-elif(randomNumber >= 0.85):
-    functionTwo(5,5)
-else:
-    functionThree(5,5)
-    functionFour(5,5)
+for function in randomFunctionGenerator:
+    returnedList = function(x, y)
+    x = returnedList[0]
+    y = returnedList[1]
+    xValues.append(x)
+    yValues.append(y)
 
-print(functionOne(5, 5))
-print(functionTwo(5, 5))
-print(functionThree(5,5))
-print(functionFour(5, 5))
+plt.axes().set_aspect("equal")
+plt.scatter(xValues, yValues, color="green", s= 0.05)
+plt.show()
